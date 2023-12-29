2107e7c89504c7d915bafcff361bd40873c6956b
Aleksejs <aleksf87+gh@gmail.com>
2023-12-29 22:54:37 +0200

    Change avatar provider to DiceBear

---
+++ app/Filament/AvatarProviders/DiceBearAvatarsProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\AvatarProviders;
+
+use Filament\AvatarProviders\Contracts;
+use Filament\Facades\Filament;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class DiceBearAvatarsProvider implements Contracts\AvatarProvider
+{
+    public function get(Model | Authenticatable $record): string
+    {
+        $name = str(Filament::getNameForDefaultAvatar($record))
+            ->trim()
+            ->explode(' ')[0];
+
+        return 'https://api.dicebear.com/7.x/thumbs/svg?seed=' . urlencode($name);
+    }
+}
