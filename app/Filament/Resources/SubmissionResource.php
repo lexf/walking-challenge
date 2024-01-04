ddc2ddbcc4e18d86fd259bd0555ba91567d2826b
Aleksejs <aleksf87+gh@gmail.com>
2024-01-04 08:49:23 +0200

    Add basic models and resources for tracking event and submissions

---
+++ app/Filament/Resources/SubmissionResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubmissionResource\Pages;
+use App\Filament\Resources\SubmissionResource\RelationManagers;
+use App\Models\Submission;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SubmissionResource extends Resource
+{
+    protected static ?string $model = Submission::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-arrow-up-on-square-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('distance')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\DatePicker::make('date')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('distance')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSubmissions::route('/'),
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        // Hardcode the number of submissions for now.
+        // TODO: Implement a way to get the number of pending submissions.
+        return 10;
+    }
+
+    public static function getNavigationBadgeColor(): ?string
+    {
+        return 'danger';
+    }
+}
---
+++ app/Filament/Resources/SubmissionResource/Pages/ManageSubmissions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\SubmissionResource\Pages;
+
+use App\Filament\Resources\SubmissionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageSubmissions extends ManageRecords
+{
+    protected static string $resource = SubmissionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
