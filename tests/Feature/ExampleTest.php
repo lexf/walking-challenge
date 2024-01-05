49b89fdc86bab40a7aaafc954a2386e176912de1
Aleksejs <aleksf87+gh@gmail.com>
2024-01-05 21:54:40 +0200

    Show Filament app by default

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,6 @@
 it('returns a successful response', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    // returning redirect to login page
+    $response->assertStatus(302);
 });
